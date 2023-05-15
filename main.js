73fd68ceba490fd4fd03a9003cc659cb92309b53
edvanbarbosa <edvan.santos.102@academico.ifs.edu.br>
2023-05-15 16:44:15 -0300

    js

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-document.querySelector(".style-theme").href = "theme-1"
+const buttons = document.querySelectorAll(".buttons .button")
+console.log(buttons)
+
+const visor = document.querySelector(".visor")
+visor.innerHTML =""
+
+
+const buttonPad = (tecla)=>{
+
+    let clasName = tecla.className
+    const special_key = clasName == "button del" || clasName == "button reset" || clasName == "button equal"? true:false
+
+    
+    if (special_key){
+        
+        if(clasName == "button del"){
+            visor.innerHTML.charAt(visor.innerHTML.length-1) = ""
+
+        }
+
+    }
+    else{
+        visor.innerHTML = visor.innerHTML + tecla.innerHTML
+    }
+     
+
+}
+
+buttons.forEach(e =>{
+    e.addEventListener("click",()=>{
+        buttonPad(e)
+    })
+})

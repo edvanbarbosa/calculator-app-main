eaff3224697a506a34d2bb50c12c3b6894ad4b6e
Edvan Barbosa <114129267+edvanbarbosa@users.noreply.github.com>
2023-05-19 16:54:49 -0300

    js-reset

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 const buttons = document.querySelectorAll(".buttons .button")
-console.log(buttons)
 
 const visor = document.querySelector(".visor")
-visor.innerHTML =""
-
 
 const buttonPad = (tecla)=>{
-
     let clasName = tecla.className
     const special_key = clasName == "button del" || clasName == "button reset" || clasName == "button equal"? true:false
 
@@ -14,17 +10,28 @@ const buttonPad = (tecla)=>{
     if (special_key){
         
         if(clasName == "button del"){
-            visor.innerHTML.charAt(visor.innerHTML.length-1) = ""
+            visor.innerHTML = visor.innerHTML.slice(0,visor.innerHTML.length-1)
 
+        }
+        else if(clasName == "button reset"){
+            visor.innerHTML = ""
+        }
+        else{
+           equal()
         }
 
     }
     else{
         visor.innerHTML = visor.innerHTML + tecla.innerHTML
     }
+
+    const equal = ()=>{
+        
+    }
      
 
 }
+
 
 buttons.forEach(e =>{
     e.addEventListener("click",()=>{
